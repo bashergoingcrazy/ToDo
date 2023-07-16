ace9d8510a0499c1d2fb1ea011c3e3946c8d3463
bashergoingcrazy <anjusareene4@gmail.com>
2023-07-17 04:11:43 +0530

    First day at work completed html and half css will finish this project tomorrow

--- index.js
+++ index.js
@@ -5,12 +5,18 @@ const app = express();
 //Intializing the Port 
 const port = 8000;
 
+
+
+
 //Manages the routes in our app
 app.use('/',require('./routes'));
 
 //Setting up engine that we use
 app.set('view engine', 'ejs');
 app.set('views','./views');
+//Setting up the access of css and js files 
+app.use(express.static('assets'));
+
 
 
 //Start our server via listening to port
